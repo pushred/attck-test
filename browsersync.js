892ea791c17bc909e92743e6bb0d45e6b806196e
Eric Lanehart <eric@pushred.co>
2017-03-22 09:32:35 -0400

    Style UI
    Postprocess with PostCSS / cssnext. We don't actually need everything cssnext provides but Codepen doesn't offer the custom-properties plugin individually.

--- browsersync.js
+++ browsersync.js
@@ -3,7 +3,7 @@ const historyApiFallback = require('connect-history-api-fallback');
 
 browserSync.init({
   server: true,
-  files: ['index.html', 'index.css', 'bundle.js'],
+  files: ['index.html', 'bundle.css', 'bundle.js'],
   middleware: [historyApiFallback()],
   notify: false,
   open: false
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const UID = window.shortid.generate;
-const {BrowserRouter, Route, Link} = window.ReactRouterDOM;
+const {BrowserRouter, Route, NavLink} = window.ReactRouterDOM;
 const {kebabcase, sortby} = window.lodash;
 
 let sortBy = sortby;
 
-const data = {
+const DATA = {
   "characters": [{
     "name": "Luke Skywalker",
     "url": "https://swapi.co/api/people/1/"
@@ -20,8 +20,18 @@ const data = {
   }]
 };
 
+const ROMANS = {
+  1: 'I',
+  2: 'II',
+  3: 'III',
+  4: 'IV',
+  5: 'V',
+  6: 'VI',
+  7: 'VII'
+};
+
 function App () {
-  let characterLinks = data.characters.reduce((links, character) => {
+  let characterLinks = DATA.characters.reduce((links, character) => {
     let id = parseId(character.url);
     let slug = kebabcase(character.name);
 
@@ -34,24 +44,31 @@ function App () {
   return (
     <BrowserRouter>
       <main className="Main">
-        <header>Star Wars</header>
+        <h1 className="Brand">SWDB</h1>
 
-        <ul className="CharacterList">
-          {characterLinks.map(props => <Character key={UID()} {...props} />)}
-        </ul>
+        <div className="Main__content">
+          <section className="Characters">
+            <h2 className="Heading">Choose a Character</h2>
+            <ul className="CharacterList">
+              {characterLinks.map(props => <Character key={UID()} {...props} />)}
+            </ul>
+          </section>
 
-        <Route path="/:id/:slug" component={MovieList} />
-
+          <Route path="/:id/:slug" component={MovieList} location={location} key={location.key} />
+        </div>
       </main>
     </BrowserRouter>
   );
 }
 
 function Character ({name, path}) {
+  var className = 'CharacterList__item Character';
+  if (!path) className += ' Character--error';
+
   return (
-    <li className="CharacterList__item Character">
+    <li className={className}>
       {path
-        ? <Link className="Character__link" to={path}>{name}</Link>
+        ? <NavLink className="Character__link" activeClassName="Character__link--active" to={path}>{name}</NavLink>
         : name}
     </li>
   );
@@ -76,14 +93,21 @@ class MovieList extends React.Component {
       .then(data => {
         movieIds = data.films.map(url => parseId(url));
 
-        return Promise.all(
-          data.films
+        let requests = data.films
             // only fetch films if they haven't been already
             .filter(url => !Object.keys(this.state.movies).includes(parseId(url)))
-            .map(url => fetch(url).then(res => res.json()))
-        );
+            .map(url => fetch(url).then(res => res.json()));
+
+        if (requests) {
+          document.body.classList.add('is-loading');
+          return Promise.all(requests);
+        } else {
+          return Promise.all();
+        }
       })
       .then(data => {
+        document.body.classList.remove('is-loading');
+
         let movies = Object.assign({},
             this.state.movies,
             data.reduce((movies, movie) => {
@@ -104,41 +128,44 @@ class MovieList extends React.Component {
         // data is fetched once, then cached in component state
         this.setState({
           characters: Object.assign(this.state.characters, {
-            [characterId]: movieIdsByEpisode
+            [characterId]: {
+              name: DATA.characters.find(c => parseId(c.url) === characterId).name,
+              movies: movieIdsByEpisode
+            }
           }),
           movies
-        })
+        });
       })
       .catch(alert);
   }
 
   render () {
     let {id} = this.props.match.params;
-    let movieList = this.state.characters[id];
 
-    if (!movieList) {
+    if (!this.state.characters[id]) {
       this.getMovies(id);
-      return <em>Loading</em>;
+      return null; // intentionally optimistic loader
     }
 
-    let movieData = movieList.map(id => this.state.movies[id]);
+    let {name, movies} = this.state.characters[id];
+    let movieData = movies.map(id => this.state.movies[id]);
 
     return (
       <section className="MovieList">
-        <h2>Filmography</h2>
         {movieData.map(props => <Movie key={UID()} {...props} />)}
       </section>
     );
   }
 }
 
-function Movie ({title, releaseDate}) {
+function Movie ({title, releaseDate, episodeId}) {
   // PST timezone offset avoids inconsistencies due to local tz conversion
   let formattedReleaseDate = dateFns.format(releaseDate + 'T00:00:00−7:00', 'dddd, MMMM D YYYY');
 
   return (
-    <section className="MovieList__item Movie">
+    <section className={`MovieList__item Movie Movie--episode-${episodeId}`}>
       <h3 className="Movie__title">{title}</h3>
+      <h4 className="Movie__episode" title={"Episode " + episodeId}>{ROMANS[episodeId]}</h4>
       <time className="Movie__release" dateTime={releaseDate}>{formattedReleaseDate}</time>
     </section>
   );
